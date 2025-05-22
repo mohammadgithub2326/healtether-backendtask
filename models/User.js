6223528ce13dbf18a96fa89f6b1f7d59c7e472bc
mohammadgithub2326 <132339501+mohammadgithub2326@users.noreply.github.com>
2025-05-22 18:00:33 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
 
+console.log("entered models");
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validator.isEmail, 'Invalid email'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+console.log("exiting  models");
+
+module.exports = mongoose.model('User', userSchema);

35fbda0cd46909ab0c7350a02a6e105e596d58d5
mohammadgithub2326 <132339501+mohammadgithub2326@users.noreply.github.com>
2025-05-24 11:51:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 console.log("routes working")
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth.js'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
